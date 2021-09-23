b669f02fbe3d6860ebbf82003d8888d162df8e0c
Test <matonikolov77@yahoo.com>
2021-09-23 10:30:48 +0200

    Inicijaliziran prazen proekt

---
+++ Pottrcko/Pottrcko/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pottrcko.Startup))]
+namespace Pottrcko
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
